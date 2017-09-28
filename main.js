35225f84c9c19b9603a67b4a11ca8e44bac6e691
TRI VI <trivi2710@gmail.com>
2017-09-27 22:50:02 -0400

    upload image section using mutter

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var storage = multer.diskStorage({
@@ -6,11 +7,11 @@ var storage = multer.diskStorage({
 		cb(null, 'public/img')
 	},
 	filename: function(req, file, cb){
-		cb(null, file.originalname + '-' + Date.now());
+		cb(null, file.originalname);
 	}
 });
 var upload = multer({storage: storage}).single('imageFile');
-var app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -24,10 +25,25 @@ app.use(bodyParser.json())
 
 //APP
 
-app.get('/', function(req, rest){
+app.get('/', function(req, res){
 	res.render('mainpage');
 });
+
+app.post('/upload', function(req, res){
+	upload(req, res, function(err){
+		if(err){
+			console.log(err);
+			return;
+		}
+		var imageFile = req.file.size;
+		console.log(imageFile);
+		res.send({
+			fileSize: imageFile
+		});
+	});
+});
+
 app.listen(process.env.PORT || 3000, function(){
 	console.log('listening on port 3000');
-})
+});
 
